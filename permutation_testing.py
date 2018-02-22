rank degeneracy, rotation bug

permutation_testing.py
@@ -1,7 +1,13 @@
+from __future__ import print_function
+
 from numpy import * 
 import numpy.linalg
 import numpy as np
 import albi_lib
+import importlib
+import sys
+import scipy.stats
+
 
 import os.path
 curdir = os.path.dirname(os.path.realpath(__file__))
@@ -10,7 +16,16 @@ import sys
 if os.path.join(curdir, "pylmm") not in sys.path:
     sys.path.insert(0, os.path.join(curdir, "pylmm"))
 import pylmm.lmm_unbounded
-reload(pylmm.lmm_unbounded)
+
+
+import albi_lib
+import samc
+
+if sys.version_info.major == 3:
+    importlib.reload(pylmm.lmm_unbounded)
+    importlib.reload(samc)
+    importlib.reload(albi_lib)
+
 
 from tqdm import *
 
@@ -19,11 +34,23 @@ from tqdm import *
 # Parametric testing
 #
 
-def parametric_testing(y, kinship_matrix, kinship_eigenvectors, kinship_eigenvalues, covariates=None):
+def parametric_testing(y, kinship_matrix, kinship_eigenvectors, kinship_eigenvalues, covariates=None, pylmm_resolution=100, cutoff=1e-5):
     if covariates is None:
         covariates = ones_like(y[:,newaxis])
-    obj = pylmm.lmm_unbounded.LMM(y[:,newaxis], kinship_eigenvectors, kinship_eigenvalues, kinship_eigenvectors, X0=covariates)
-    res = obj.fit(REML=True) 
+
+    # Make sure the eigenvalues are in decreasing order and nonzero
+    reverse_sorted_indices = argsort(kinship_eigenvalues)[::-1]
+    kinship_eigenvalues = array(kinship_eigenvalues)[reverse_sorted_indices]
+    kinship_eigenvectors = array(kinship_eigenvectors)[:, reverse_sorted_indices]
+
+    n_effective = len(where(kinship_eigenvalues >= cutoff)[0])
+    kinship_eigenvalues =kinship_eigenvalues[:n_effective]
+
+    rotated_covariates = dot(kinship_eigenvectors.T, covariates)[:n_effective,:]
+    rotated_y = dot(kinship_eigenvectors.T, y)[:n_effective]
+
+    obj = pylmm.lmm_unbounded.LMM(rotated_y[:,newaxis], identity(n_effective), kinship_eigenvalues, identity(n_effective), X0=rotated_covariates)
+    res = obj.fit(REML=True, explicit_H=linspace(0,1,pylmm_resolution+1))
     return res[0], 0.5*scipy.stats.distributions.chi2.sf(2*(res[3]-obj.LLs[0]), 1)
 
 
@@ -52,9 +79,9 @@ def permute_columns(x, permutation_blocks, seed):
 # Derivative-based permutation testing
 #
 
-def permutation_testing_only_eigenvectors(y, h2_estimate, kinship_eigenvectors, kinship_eigenvalues, n_permutations, permutation_blocks=None, max_memory_in_gb=None, seed=None, verbose=False):
+def permutation_testing_only_eigenvectors(y, h2_estimate, kinship_eigenvectors, kinship_eigenvalues, n_permutations, permutation_blocks=None, max_memory_in_gb=None, seed=None, cutoff=1e-5, verbose=False):
     n = len(kinship_eigenvalues)
-    der = albi_lib.OnlyEigenvectorsDerivativeSignCalculator([0], [h2_estimate], kinship_eigenvalues, eigenvectors_as_X=[-1])
+    der = albi_lib.OnlyEigenvectorsDerivativeSignCalculator([0], [h2_estimate], kinship_eigenvalues, eigenvectors_as_X=[-1], cutoff=cutoff)
 
     if max_memory_in_gb is not None:
         chunk_size = max(1, int((max_memory_in_gb * 2.0**30)/ (n * 64)))
@@ -65,11 +92,14 @@ def permutation_testing_only_eigenvectors(y, h2_estimate, kinship_eigenvectors, 
         n_permutations_in_chunk = min(n_permutations, n_chunk + chunk_size) - n_chunk
         permuted = permute_columns(repeat(y[:,newaxis], n_permutations_in_chunk, 1), permutation_blocks, (seed + n_chunk if seed is not None  else None))
 
-        p.append(der.get_derivative_signs(permuted.T[:,newaxis,:])[:,0,0] >= 0)
+        rotated = np.dot(kinship_eigenvectors.T, permuted)
+
+        p.append(der.get_derivative_signs(rotated.T[:,newaxis,:])[:,0,0] >= 0)
+        
           
     return sum(concatenate(p))
 
-def permutation_testing(y, h2_estimate, kinship_eigenvectors, kinship_eigenvalues, covariates, n_permutations, permutation_blocks=None, max_memory_in_gb=None, seed=0, verbose=False):
+def permutation_testing(y, h2_estimate, kinship_eigenvectors, kinship_eigenvalues, covariates, n_permutations, permutation_blocks=None, max_memory_in_gb=None, seed=0, cutoff=1e-5, verbose=False):
     n = len(kinship_eigenvalues)
     
     if max_memory_in_gb is not None:
@@ -90,10 +120,36 @@ def permutation_testing(y, h2_estimate, kinship_eigenvectors, kinship_eigenvalue
         permuted_X = array(l)
         
         for i in range(n_permutations_in_chunk):
-            der = albi_lib.GeneralDerivativeSignCalculator([0], [h2_estimate], kinship_eigenvalues, kinship_eigenvectors, permuted_X[:, :, i].T)
+            der = albi_lib.GeneralDerivativeSignCalculator([0], [h2_estimate], kinship_eigenvalues, kinship_eigenvectors, permuted_X[:, :, i].T, cutoff=cutoff)
             p.append(der.get_derivative_signs(permuted_y[newaxis, :, i]) >= 0)
           
     return mean(concatenate(p))
+
+def naive_permutation_testing(y, h2_estimate, kinship_matrix, kinship_eigenvectors, kinship_eigenvalues, covariates, n_permutations, permutation_blocks=None, max_memory_in_gb=None, seed=0, verbose=False):
+    n = len(kinship_eigenvalues)
+    
+    if max_memory_in_gb is not None:
+        chunk_size = max(1, int((max_memory_in_gb * 2.0**30)/ (n * 64)))
+    else:
+        chunk_size = n_permutations
+
+    p = []
+    for n_chunk in (range(0, n_permutations, chunk_size)):
+        n_permutations_in_chunk = min(n_permutations, n_chunk + chunk_size) - n_chunk
+        permuted_y = permute_columns(repeat(y[:,newaxis], n_permutations_in_chunk, 1), permutation_blocks,  seed + n_chunk)
+        
+        l = []
+        for c in range(shape(covariates)[1]):
+            a = permute_columns(repeat(covariates[:,c:(c+1)], n_permutations_in_chunk, 1), permutation_blocks, seed + n_chunk) 
+            l.append(a)
+
+        permuted_X = array(l)
+        
+        for i in tnrange(n_permutations_in_chunk, disable=(not verbose)):
+            h2, param_p = parametric_testing(permuted_y[:, i], kinship_matrix, kinship_eigenvectors, kinship_eigenvalues, permuted_X[:, :, i].T)            
+            p.append(h2 >= h2_estimate)
+          
+    return mean(p)    
 
 ################################################################################################
 # SAMC with ALBI
@@ -101,11 +157,89 @@ def permutation_testing(y, h2_estimate, kinship_eigenvectors, kinship_eigenvalue
 import scipy.optimize, scipy.interpolate
 import sys
 
+def samc_heritability_only_eigenvectors(x0, n_partitions, n_iterations, kinship_eigenvalues, kinship_eigenvectors, 
+                                        replace_proportion=0.05, relative_sampling_error_threshold=0.2, 
+                                        t0=1000):
+
+    def heritability_test_statistic(x, current_partition, kinship_eigenvectors, partitions, derivative_calculator):
+        rotated_x = np.dot(kinship_eigenvectors.T, x)
+        ds = derivative_calculator.get_derivative_signs(rotated_x[newaxis, newaxis, :])[0,0,:]
+
+        # First check if we are in the same partition
+        if current_partition == 0 and ds[0] <= 0:
+            part = current_partition      
+            pp = [0, partitions[0]]
+        elif current_partition == len(partitions) and ds[-1] >= 0:
+            part = current_partition
+            pp = [partitions[-1], 1]
+        elif ds[current_partition-1] >= 0 and ds[current_partition] <= 0:
+            part = current_partition
+            pp = partitions[part-1:part+1].tolist()
+
+        # If not, find the new partition
+        else:
+            if ds[0] <= 0:
+                part = 0
+                pp = [0, partitions[0]]
+            elif ds[-1] >= 0:
+                part = len(ds)
+                pp = [partitions[-1], 1]
+            else:
+                w = where((ds[:-1] >= 0) & (ds[1:] <= 0))[0]
+                if len(w):
+                    part = w[0]+1
+                    pp = partitions[w[0]:w[0]+2].tolist()
+                else:
+                    assert "Should not happen"
+
+        return pp, part
+
+    def heritability_generate_sample(x):
+        L = int(replace_proportion*len(x))
+        indices = np.random.choice(range(len(x)), L, replace=False)
+        permuted_indices = np.random.permutation(indices)
+        y = x.copy()
+        y[indices] = y[permuted_indices]
+        return y, 1.0
+
+    K = np.linalg.multi_dot([kinship_eigenvectors, diag(kinship_eigenvalues), kinship_eigenvectors.T])
+
+    res0 = parametric_testing(x0, K, kinship_eigenvectors, kinship_eigenvalues)
+    print("Estimated h^2:", res0[0])
+    
+    observed_statistic = res0[0]
+
+
+    partitions = concatenate([arange(0, observed_statistic, observed_statistic/n_partitions)[1:], [observed_statistic]])
+
+    #weights_at_partitions = albi_lib.weights_zero_derivative([0], partitions, kinship_eigenvalues)[0, :, :]
+
+    derivative_calculator = albi_lib.OnlyEigenvectorsDerivativeSignCalculator([0], partitions, kinship_eigenvalues, eigenvectors_as_X=[-1])
+
+    n_partitions_total = n_partitions + 1
+
+    theta0 = log([0.5] + [0.5/(n_partitions_total-1)]*(n_partitions_total-1))
+    theta0 -= mean(theta0)
+
+    thetas, observed_sampling_distribution, statistics = samc.SAMC_simple(samc.SAMCSimpleParameters(
+        x0=x0, 
+        test_statistic_func=lambda x, current_partition: heritability_test_statistic(x, current_partition, kinship_eigenvectors, 
+                                                                                     partitions, derivative_calculator), 
+        generate_sample_func=heritability_generate_sample, 
+        n_partitions=n_partitions_total, 
+        n_iterations=n_iterations, 
+        relative_sampling_error_threshold=relative_sampling_error_threshold,
+        t0=t0))
+
+    #return thetas, observed_sampling_distribution, statistics
+    return exp(thetas[-1])/sum(exp(thetas))
+
+
 
 def samc_heritability(x0, n_partitions, n_iterations, kinship_eigenvalues, kinship_eigenvectors, 
-                      replace_proportion=0.05, n_permutations=100000, relative_sampling_error_threshold=0.2, 
-                      random_x0=False, t0=1000, just_return_partitions=False, theta0=None, use_theta=None, init_run_iterations=0, 
-                      second_run_partitions=0, polyak_ruppert=False):
+                      replace_proportion=0.05, relative_sampling_error_threshold=0.2, 
+                      t0=1000):
+
     def heritability_test_statistic(x, current_partition, kinship_eigenvectors, partitions, weights_at_partitions):
         rotated = dot(kinship_eigenvectors.T, x)
         
@@ -150,74 +284,51 @@ def samc_heritability(x0, n_partitions, n_iterations, kinship_eigenvalues, kinsh
         y[indices] = y[permuted_indices]
         return y, 1.0
 
-    res0 = pylmm_estimate(x0, kinship_eigenvalues, kinship_eigenvectors)
-    print "Estimated h^2:", res0[0]
-    #ps = albi_permutation_testing(x0, res0[0], kinship_eigenvectors, kinship_eigenvalues, n_permutations, max_memory_in_gb=0.1, verbose=True)
-    #print "Permutation testing p:", mean(ps)
+    K = np.linalg.multi_dot([kinship_eigenvectors, diag(kinship_eigenvalues), kinship_eigenvectors.T])
 
+    res0 = parametric_testing(x0, K, kinship_eigenvectors, kinship_eigenvalues)
+    print("Estimated h^2:", res0[0])
+    
     observed_statistic = res0[0]
 
 
     partitions = concatenate([arange(0, observed_statistic, observed_statistic/n_partitions)[1:], [observed_statistic]])
 
-    if just_return_partitions:
-        return partitions
-
-
-    weights_at_partitions = albi_lib.albi_lib.weights_zero_derivative([0], partitions, kinship_eigenvalues)[0, :, :]
+    weights_at_partitions = albi_lib.weights_zero_derivative([0], partitions, kinship_eigenvalues)[0, :, :]
 
     n_partitions_total = n_partitions + 1
 
-    if theta0 is None:
-        theta0 = log([0.5] + [0.5/(n_partitions_total-1)]*(n_partitions_total-1))
-        theta0 -= mean(theta0)
+    theta0 = log([0.5] + [0.5/(n_partitions_total-1)]*(n_partitions_total-1))
+    theta0 -= mean(theta0)
 
-    if random_x0:
-        x0 = np.random.permutation(x0)
-
-    thetas, observed_sampling_distribution, statistics = SAMC_simple(SAMCSimpleParameters(
+    thetas, observed_sampling_distribution, statistics = samc.SAMC_simple(samc.SAMCSimpleParameters(
         x0=x0, 
         test_statistic_func=lambda x, current_partition: heritability_test_statistic(x, current_partition, kinship_eigenvectors, 
                                                                                      partitions, weights_at_partitions), 
         generate_sample_func=heritability_generate_sample, 
         n_partitions=n_partitions_total, 
-        n_iterations=(init_run_iterations if init_run_iterations > 0 else n_iterations), 
-        relative_sampling_error_threshold=relative_sampling_error_threshold,
-        t0=t0,
-        theta0=theta0,
-        fix_theta=(use_theta is not None),
-        step_size_power=(0.75 if polyak_ruppert else 1.0)))
-
-    if init_run_iterations == 0:
-        return thetas, observed_sampling_distribution, statistics
-
-    n_partitions_total = second_run_partitions+1
-
-    fhat = exp(thetas)/sum(exp(thetas))
-    Fhat = add.accumulate(fhat)
-    delta = (1-Fhat[-2])
-    bestr = scipy.optimize.brentq(lambda r: delta*sum([r**(n_partitions_total-1-k) for k in range(1,n_partitions_total)]) - Fhat[-2], 0, int(sys.float_info.max) ** (1.0/(n_partitions_total-1)))
-    Fhat_interpolated = scipy.interpolate.interp1d([0]+list(partitions), [0]+list(Fhat[:-1]))
-    accr = delta*add.accumulate([bestr**(n_partitions_total-1-k) for k in range(1, n_partitions_total)])
-    newpartitions = array([scipy.optimize.brentq(lambda x: Fhat_interpolated(x) - p, 0, partitions[-1]) for p in accr[:-1]] + [partitions[-1]])
-
-    theta0 = concatenate([[log(delta*bestr**(n_partitions_total-1-k)) for k in range(1, n_partitions_total)], [log(delta)]])
-    theta0 -= mean(theta0)
- 
-    weights_at_new_partitions = albi_lib.albi_lib.weights_zero_derivative([0], newpartitions, kinship_eigenvalues)[0, :, :]
-
-    return SAMC_simple(SAMCSimpleParameters(
-        x0=x0, 
-        test_statistic_func=lambda x, current_partition: heritability_test_statistic(x, current_partition, kinship_eigenvectors, newpartitions, weights_at_new_partitions), 
-        generate_sample_func=heritability_generate_sample, 
-        n_partitions=n_partitions_total, 
         n_iterations=n_iterations, 
         relative_sampling_error_threshold=relative_sampling_error_threshold,
-        t0=t0,
-        theta0=theta0,
-        fix_theta=(use_theta is not None),
-        step_size_power=(0.75 if polyak_ruppert else 1.0)))
+        t0=t0))
+
+    #return thetas, observed_sampling_distribution, statistics
+    return exp(thetas[-1])/sum(exp(thetas))
+
+
+
+def draw_multivariate(X, times=1, random_seed=None):
+    return dot(X, numpy.random.RandomState(random_seed).randn(shape(X)[0], times))
+
+def draw_multivariate_from_eigen(U, eigvals, times=1, random_seed=None):
+    return draw_multivariate(U * (maximum(eigvals, 0)**0.5)[newaxis, :], times, random_seed)
+
 
 def samc_heritability_sim(n_partitions, n_iterations, h2, kinship_eigenvalues, kinship_eigenvectors, replace_proportion=0.05, relative_sampling_error_threshold=0.2):
     y = draw_multivariate_from_eigen(kinship_eigenvectors, h2*kinship_eigenvalues + (1-h2))[:, 0]
-    return samc_heritability(y, n_partitions, n_iterations, kinship_eigenvalues, kinship_eigenvectors, replace_proportion, relative_sampling_error_threshold)
+    print(samc_heritability(y, n_partitions, n_iterations, kinship_eigenvalues, kinship_eigenvectors, replace_proportion, relative_sampling_error_threshold))
+    print(samc_heritability_only_eigenvectors(y, n_partitions, n_iterations, kinship_eigenvalues, kinship_eigenvectors, replace_proportion, relative_sampling_error_threshold))
+    K = np.linalg.multi_dot([kinship_eigenvectors, diag(kinship_eigenvalues), kinship_eigenvectors.T])
+    res = parametric_testing(y, K, kinship_eigenvectors, kinship_eigenvalues)
+    print(res)
+    print(permutation_testing_only_eigenvectors(y, res[0], kinship_eigenvectors, kinship_eigenvalues, 10000, verbose=True)/10000.0)
+